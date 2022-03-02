e35fa6cc22d2d62e681d1e56133ac7614a4a49a0
dinhnamkhoa <dinhnamkhoa@gmail.com>
2022-03-02 14:02:08 +0700

    Update 1

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello Anh Khoa");
+		get("/hello", (req, res) -> "Hello Anh Khoa Dep Trai");
 	}
 }
